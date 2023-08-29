98f725179bf3594e676d3acf117526c97a0f214f
Rishik Chakravarty <rishikrc3@gmail.com>
2023-08-29 06:15:14 +0530

    basic_design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
-import Signup from "./Signup"
+import Signup from "./Signup";
 
 function App() {
- 
-
   return (
     <>
-      <h1>Hemlo</h1>
       <Signup></Signup>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Card, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
       <div
@@ -17,23 +19,35 @@ const Signup = () => {
           Welcome to Coursera. Sign up below
         </Typography>
       </div>
-
-      <center>
-        <div
-          style={{
-            border: "2px solid black",
-            width: 400,
-          }}
-        >
-          Username
-          <input type="text" />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card varint={"outlined"} style={{ width: 400, padding: 20 }}>
+          <TextField
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            fullWidth={true}
+            label="Email"
+            variant="outlined"
+          />
           <br />
-          Password
-          <input type="password" />
           <br />
-          <Button variant="contained">Singup</Button>
-        </div>
-      </center>
+          <TextField
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            fullWidth={true}
+            label="Password"
+            variant="outlined"
+            type={"password"}
+          />
+          <br />
+          <br />
+          <Button size={"large"} variant="contained">
+            {" "}
+            Signup
+          </Button>
+        </Card>
+      </div>
     </>
   );
 };
