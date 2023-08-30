0ef764ae211e7a546b92b5802d35d880f5caaecd
Rishik Chakravarty <rishikrc3@gmail.com>
2023-08-30 17:45:54 +0530

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import Signup from "./Signup";
 import Appbar from "./Appbar";
-
+import Signin from "./Signin";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <div
         style={{ width: "100vw", height: "100vh", backgroundColor: "#e2e2e2" }}
       >
-        <Signup />
-        <Appbar />
+        <Router>
+          {" "}
+          <Appbar />
+          <Routes>
+            <Route path={"/signup"} element={<Signup />} />
+            <Route path={"/signin"} element={<Signin />} />
+          </Routes>
+        </Router>
       </div>
     </>
   );
--- frontend/src/Appbar.jsx
+++ frontend/src/Appbar.jsx
@@ -1,9 +1,41 @@
 import React from "react";
-
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
 const Appbar = () => {
   return (
-    <div>
-      <h1>Hemlo</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: 4,
+      }}
+    >
+      <div style={{ marginLeft: 10 }}>
+        <Typography variant={"h6"}>Coursera</Typography>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div>
+          <Button
+            variant={"contained"}
+            style={{ marginRight: 10 }}
+            onClick={() => {
+              window.location = "/signup";
+            }}
+          >
+            Signup
+          </Button>
+        </div>
+        <div>
+          <Button
+            variant={"contained"}
+            onClick={() => {
+              window.location = "/signin";
+            }}
+          >
+            Signup
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
