b6a859da495dceda1801a39672fb58188186a3b2
Rishik Chakravarty <rishikrc3@gmail.com>
2023-08-30 16:26:00 +0530

    sample

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import Signup from "./Signup";
+import Appbar from "./Appbar";
 
 function App() {
   return (
     <>
-      <Signup></Signup>
+      <div
+        style={{ width: "100vw", height: "100vh", backgroundColor: "#e2e2e2" }}
+      >
+        <Signup />
+        <Appbar />
+      </div>
     </>
   );
 }
---
+++ frontend/src/Appbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Appbar = () => {
+  return (
+    <div>
+      <h1>Hemlo</h1>
+    </div>
+  );
+};
+
+export default Appbar;
