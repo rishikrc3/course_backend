1491b53e4b501c7b9d7e26581b0a528eddaa3e6b
Rishik Chakravarty <rishikrc3@gmail.com>
2023-09-01 18:34:15 +0530

    displaying_courses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Signup from "./Signup";
 import Appbar from "./Appbar";
 import Signin from "./Signin";
 import Addcourse from "./Addcourse";
+import Courses from "./Courses";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path={"/addcourse"} element={<Addcourse />} />
             <Route path={"/signup"} element={<Signup />} />
             <Route path={"/signin"} element={<Signin />} />
+            <Route path={"/courses"} element={<Courses />} />
           </Routes>
         </Router>
       </div>
